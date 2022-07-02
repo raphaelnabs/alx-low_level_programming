fabd077eb3eb0c7b6243c453505d4dc84d6fbd11
Raphael Nnabuenyi <nnabuenyi2017@gmail.com>
2022-07-02 13:28:15 -0700

    gitsize

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/** main -Entry point
+/** 
+ * main -Entry point
  * Return Always 0 (success)
  */
 int main(void)
@@ -16,3 +17,4 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", sizeof(d));
 	return (0);
 }
+
