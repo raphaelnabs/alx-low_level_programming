e4441049e0a29b150ebf9ef30561f469994db828
Raphael Nnabuenyi <nnabuenyi2017@gmail.com>
2022-07-03 14:23:34 -0700

    6size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	long long int ll;
 	float d;
 	char c;
-	printf("Size of a char: %lu byte(s)\n\n", sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
