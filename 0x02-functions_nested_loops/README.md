03445da636ca440534b7024bf2ad92e823306d64
Raphael Nnabuenyi <nnabuenyi2017@gmail.com>
2022-07-05 10:49:36 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+project about nested loops
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+int _putchar(char k);
+
+void print_alphabet(void);
+
+void print_alphabetx10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int a);
+
+int print_last_digit(int a);
+
+void jack_bauer(void);
+
+void times_tabe(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
