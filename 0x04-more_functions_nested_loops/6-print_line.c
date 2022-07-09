40278e586d92adfbbdd4dcc1910842ebc7cd4ff2
Raphael Nnabuenyi <nnabuenyi2017@gmail.com>
2022-07-08 17:39:25 -0700

    hot fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,10 +3,17 @@
 /**
  * print_line - printing line
  * @n: integer params
- *
  */
 void print_line(int n)
 {
 	int x;
 
-	if (n <= 0);
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x <= n; x++)
+		_putchar(95);
+	_putchar('\n');
+	}
+}
